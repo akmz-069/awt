9a121040e7e930395d4aa822041f573321f5a115
akmz-069 <aayush0mulmi@gmail.com>
2025-05-17 17:00:57 +0545

    feat: add /quotes endpoint with sample quote data

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,27 @@
 const express = require('express')
-const app = express()
+const app = express() 
 const port = 3000
 
+app.use(express.json());
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World')
+  })
+app.get('/page', (req, res) => {
+  res.send('1st page')
+  })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+let quote = [
+    { id: 1, text: "TEXT1" },
+    { id: 2, text: "TEXT2" },
+]
+
+app.get('/quotes', (req, res) => {
+    res.json(quote)
+});
